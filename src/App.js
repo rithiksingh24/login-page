52581c0e01972699259934421a6b28a29a87fc60
rithiksingh24 <Rithik.Singh@kas-services.com>
2022-04-04 23:34:36 +0530

    logout

--- src/App.js
+++ src/App.js
@@ -6,27 +6,38 @@ import { getDefaultNormalizer } from '@testing-library/react';
 
 function App() {
   const adminUser={
-    email: "admin@gmail.com",
-    password: "admin123"
+    email:"admin@gmail.com",
+    password:"admin123"
   }
   const [user,setUser]=useState({name: "",email: ""});
   const [error,setError]=useState("");
 
   const Login = details => {
     console.log(details);
+    if (details.email==adminUser.email && details.password==adminUser.password){
+     console.log("logged in");
+     setUser({
+       name:details.name,
+       email:details.email
+     });
+    }
+    else{
+    console.log("details donot match");
+    setError("details donot match");
+     }
   }
-  const Logout=()=>{
-    console.log("Logout");
+  const Logout = () => {
+    setUser({ name: "", email: ""});
   }
   return (
     <div className="App">
       {(user.email !="")?(
         <div className='welcome'>
           <h2>Welcome,<span>{user.name}</span></h2>
-          <button>Logout</button>
+          <button onClick={Logout}>Logout</button>
     </div>
   ):(
-    <LoginForm />
+    <LoginForm Login={Login} error={error}/>
   )
     
     }
